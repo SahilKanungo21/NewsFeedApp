ff36fcac1991cda2437ad4d696b367752f2d1c46
Sahil Kanungo <kanungosahil123@gmail.com>
2023-03-11 22:49:35 +0530

    added fragment

---
+++ app/src/main/java/com/application/newsfeed/Fragments/HealthFragment.java
@@ -0,0 +1,24 @@
+package com.application.newsfeed.Fragments;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.application.newsfeed.R;
+
+public class HealthFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        @SuppressLint("InflateParams")
+        View view = inflater.inflate(R.layout.health_fragment,null);
+        return view;
+    }
+}
--- app/src/main/java/com/application/newsfeed/MainActivity.java
+++ app/src/main/java/com/application/newsfeed/MainActivity.java
@@ -1,14 +1,67 @@
 package com.application.newsfeed;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.application.newsfeed.Fragments.PagerAdapter;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    TabItem mhome , msports , mhealth, mscience , mtechnology, mentertainment;
+    PagerAdapter pagerAdapter;
+    Toolbar mtoolBar;
+
+    private boolean filterConditions(int tabPos) {
+        return tabPos==0 || tabPos==1 || tabPos==2 || tabPos==3 || tabPos==4 || tabPos ==5;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mtoolBar = findViewById(R.id.toolbar);
+        setSupportActionBar(mtoolBar);
+
+        mhome = findViewById(R.id.Home);
+        mscience = findViewById(R.id.Science);
+        mentertainment = findViewById(R.id.Entertainment);
+        mhealth = findViewById(R.id.Health);
+        msports = findViewById(R.id.Sport);
+        mtechnology = findViewById(R.id.Technology);
+
+        ViewPager viewPager = findViewById(R.id.fragmentcontainer);
+        tabLayout = findViewById(R.id.include);
+
+        pagerAdapter = new PagerAdapter(getSupportFragmentManager(),6);
+        viewPager.setAdapter(pagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                if(filterConditions(tab.getPosition())) {
+                    pagerAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
     }
 }
